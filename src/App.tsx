dd724966426f231a6108e0a33cc307b7249b72e5
Yurii Holiuk <bionicuss@gmail.com>
2020-03-19 11:47:15 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+import PhoneCatalog from './PhoneCatalog';
+import PhoneDetailsPage from './PhoneDetailsPage';
+
+const App = () => (
+  <div>
+    Hello
+  </div>
+);
+
+export default App;
